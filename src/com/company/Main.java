8770285341161231df8dbabf2f7ffe5ecb6b991c
Maria <mashanosach18@gmail.com>
2022-06-14 15:18:47 +0300

    Getting values which have duplicated.#2 (with using Set)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> list;
+        list = Arrays.asList(new Integer[]{2, 4, 3, 13, 2, 16, 12, 5, 67, 2, 4, 78, 1, 13, 67, 78});
+        getValuesThatAreDuplicated(list);
+    }
 
+    public static void getValuesThatAreDuplicated(List<Integer> numbers) {
+        Set<Integer> num = new HashSet<>();
+        for (Integer s : numbers) {
+            if ((Collections.frequency(numbers, s) > 1)) {
+                num.add(s);
+            }
+        }
+        System.out.println("The numbers which have duplicate: "
+                + num.toString().replaceAll("^\\[|\\]$", ""));
     }
 }
-
-
-
-
