ac5600844454604dd50d83ac4737b1594721c67a
Maria <mashanosach18@gmail.com>
2022-06-14 08:22:19 +0300

    Getting the most appears word in sentence.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,32 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
+    public static HashMap<String, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
+        getTheBiggestAmountOfRepeatedWords("а мама мыла Милу мылом и мыла упало");
+    }
 
+    public static void getTheBiggestAmountOfRepeatedWords(String s) {
+        String[] words = s.split(" ");
+        for (String t : words) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+        Integer maxValue = Collections.max(map.values());
+        String maxKey = null;
+        for (Map.Entry entry : map.entrySet()) {
+            if (entry.getValue() == maxValue) {
+                maxKey = (String) entry.getKey();
+            }
+        }
+        System.out.println("The most common word - " + maxKey + ". Appears in the sentence: " + maxValue);
     }
 }
+
+
+
 
 
 
