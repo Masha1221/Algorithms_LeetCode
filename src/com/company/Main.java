999e45d984e1718fe0d347f835ed0d13f1c72877
Maria <mashanosach18@gmail.com>
2022-06-16 16:06:16 +0300

    Given an integer x, return true if x is palindrome integer.
    An integer is a palindrome when it reads the same backward as forward.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int y = 121;
+        System.out.println(isPalindrome(y));
+    }
 
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String number = Integer.toString(x);
+        StringBuffer buffer = new StringBuffer(number);
+        buffer.reverse();
+        String reverseNumber = new String(buffer);
+        return reverseNumber.equals(number);
     }
 }
 
