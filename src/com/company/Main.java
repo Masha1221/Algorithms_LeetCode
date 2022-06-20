751f589abfb53e37209d1a2bf2ecd38364331fa8
Maria <mashanosach18@gmail.com>
2022-06-20 13:29:06 +0300

    Given the head of a singly linked list, return true if it is a palindrome.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode head1 = head;
+        Stack<Integer> stack = new Stack();
+        while (head1 != null) {
+            stack.push(head1.val);
+            head1 = head1.next;
+        }
+        while (!stack.isEmpty()) {
+            if (head.val != stack.pop()) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
     }
 }
 
