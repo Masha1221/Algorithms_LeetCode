47c49b53ff5f86ea71364b6f640505daa7c5fedf
Maria <mashanosach18@gmail.com>
2022-06-22 16:37:01 +0300

    Given the head of a singly linked list, return the middle node of the linked list.
    If there are two middle nodes, return the second middle node.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return slow;
+    }
 }
-
-
-
-
