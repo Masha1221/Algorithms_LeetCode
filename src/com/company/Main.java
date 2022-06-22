95cfe1e9b0bc40f32c894db79146893e49ae162f
Maria <mashanosach18@gmail.com>
2022-06-22 12:56:12 +0300

    Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        String a = "fffbfg", b = "effjfggbffjdgbjjhhdegh";
+        System.out.println(canConstruct(a, b));
+    }
 
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (map.containsKey(c) && map.get(c) != 0) {
+                map.put(c, map.get(c) - 1);
+                count++;
+            }
+        }
+        return count == ransomNote.length();
     }
 }
 
