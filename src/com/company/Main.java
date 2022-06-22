b3afca8db5d021db5daafc0895dbcb6ea8f95ef4
Maria <mashanosach18@gmail.com>
2022-06-22 15:29:01 +0300

    Given an integer num, return the number of steps to reduce it to zero.
    In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(numberOfSteps(12));
+    }
 
+    public static int numberOfSteps(int num) {
+        int countOfSteps = 0;
+        while (0 < num) {
+            if (num % 2 == 0) {
+                num /= 2;
+                countOfSteps++;
+            } else if (num % 2 != 0) {
+                num -= 1;
+                countOfSteps++;
+            }
+            if (num == 0) {
+                return countOfSteps;
+            }
+        }
+        return countOfSteps;
     }
 }
 
