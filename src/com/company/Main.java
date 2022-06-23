95902827a5afadf168ebdd0ba923ee07c46bde62
Maria <mashanosach18@gmail.com>
2022-06-23 13:41:39 +0300

    Given a non-negative integer x, compute and return the square root of x.
    Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(mySqrt(8));
+    }
 
+    public static int mySqrt(int x) {
+       int a = (int) Math.sqrt(x);
+       return a;
     }
 }
 
