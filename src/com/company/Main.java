20cd5ee350832bd5c4ca5b3fc5536d0ed22e3912
Maria <mashanosach18@gmail.com>
2022-06-23 11:13:59 +0300

    A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int[][] a = {{1, 2, 4}, {3, 2, 1}};
+        System.out.println(maximumWealth(a));
+    }
 
+    public static int maximumWealth(int[][] accounts) {
+        int n = accounts[0].length;
+        int m = accounts.length;
+        int sum = 0;
+        int max = accounts[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sum = sum + accounts[i][j];
+            }
+            if (sum > max)
+                max = sum;
+            sum = 0;
+        }
+        return max;
     }
 }
-
-
-
-
