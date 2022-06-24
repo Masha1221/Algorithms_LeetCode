1af5747c2e15e54d43db28235c172cb53703e418
Maria <mashanosach18@gmail.com>
2022-06-24 16:08:45 +0300

    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String a = "intimiccimi";
+        String b = "miccimi";
+        System.out.println(strStr(a, b));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) != needle.charAt(0)) continue;
+
+            boolean substring = true;
+
+            for (int j = i; j < i + needle.length(); j++) {
+                if (haystack.charAt(j) != needle.charAt(j - i)) substring = false;
+            }
+            if (substring) return i;
+        }
+        return -1;
     }
 }
+
+
 
 
 
