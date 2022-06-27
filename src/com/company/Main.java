5be0db2ddb1880ea65bc72eb45d86c575f46fb46
Maria <mashanosach18@gmail.com>
2022-06-27 13:53:25 +0300

    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,26 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isHelp(root.left, root.right);
+    }
+
+    public boolean isHelp(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        if (t1.val != t2.val) {
+            return false;
+        }
+        return isHelp(t1.left, t2.right) && isHelp(t1.right, t2.left);
+    }
 }
 
 
