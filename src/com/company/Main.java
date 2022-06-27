75762e35fa4d38609a7ca951efebd696232feb71
Maria <mashanosach18@gmail.com>
2022-06-27 15:35:07 +0300

    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else if (st.empty()) return false;
+            else if (c == ')' && st.pop() != '(' || c == '}' && st.pop() != '{' || c == ']' && st.pop() != '[') {
+                return false;
+            }
+        }
+        return st.empty();
+    }
 }
-
-
-
-
