6bb464217396f904b262d435fdf39277aab3e784
Maria <mashanosach18@gmail.com>
2022-06-28 14:27:55 +0300

    Given the roots of two binary trees p and q, write a function to check if they are the same or not.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if ((p == null && q != null) || (q == null && p != null))
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        boolean left = isSameTree(p.left, q.left);
+        if (!left)
+            return false;
+        boolean right = isSameTree(p.right, q.right);
+        if (!right)
+            return false;
+
+        return true;
+    }
 }
-
-
-
-
