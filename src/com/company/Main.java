bd77147e4784faac604bda559113dc068795eb4e
Maria <mashanosach18@gmail.com>
2022-06-30 08:21:11 +0300

    Given the root of a binary tree, return its maximum depth.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int a = maxDepth(root.left);
+        int b = maxDepth(root.right);
+
+        return 1 + Math.max(a, b);
+    }
 }
-
-
-
-
