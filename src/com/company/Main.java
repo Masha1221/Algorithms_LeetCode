0da24a8655091ac1ca421f9aba1172c1d30c8aed
Maria <mashanosach18@gmail.com>
2022-06-30 11:38:06 +0300

    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(convertToTitle(2));
+    }
 
+    public static String convertToTitle(int columnNumber) {
+        String a = "";
+        while (columnNumber > 0) {
+            columnNumber--;
+            int x = (columnNumber) % 26;
+            a = (char) (x + 'A') + a;
+            columnNumber /= 26;
+        }
+        return a;
     }
 }
 
