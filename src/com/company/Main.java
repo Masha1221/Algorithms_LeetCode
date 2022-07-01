398915cfcbb3a1a787319ae6b8d5f39bccbe81bc
Maria <mashanosach18@gmail.com>
2022-07-01 09:40:41 +0300

    Calculate the element that occurs the most times. Faste option. #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int[] p = new int[]{2, 1, 3, 2, 4, 5, 3, 1};
+        System.out.println(majorityElement(p));
+    }
 
+    public static int majorityElement(int[] nums) {
+        int number = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0)
+                number = nums[i];
+            if (number == nums[i])
+                count++;
+            else
+                count--;
+        }
+        return number;
     }
 }
