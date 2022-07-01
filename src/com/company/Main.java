325f8842f5aacaf698b8ce74ba9587552a68acda
Maria <mashanosach18@gmail.com>
2022-07-01 09:32:44 +0300

    Calculate the element that occurs the most times.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        int [] a = new int[]{2,3,4,2,4,3,4,2,1};
+        System.out.println(majorityElement(a));
+    }
 
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int e = 0;
+
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+            count = Math.max(count, map.get(i));
+            if (count == map.get(i)) {
+                e = i;
+            }
+        }
+        return e;
     }
 }
-
-
-
-
